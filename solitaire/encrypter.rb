1232ff8f33f3d55f7ac958bf27fd00d1d36f7f78
rbn <richnyman@gmail.com>
2012-06-15 13:41:42 -0400

    working

--- solitaire/encrypter.rb
+++ solitaire/encrypter.rb
@@ -4,7 +4,7 @@ class Encrypter
     @keystream = keystream
   end
 
-  def sanitze( s )
+  def sanitize( s )
     s = s.upcase
     s = s.gsub(/[^A-Z]/, '')
     s = s + 'X' * ((5 - s.size % 5) % 5)
@@ -20,14 +20,13 @@ class Encrypter
   end
 
   def process( s , &block )
-    s = sanitze(s)
+    s = sanitize(s)
     out = ''
     s.each_byte do |c|
       if c >= ?A.ord and c <= ?Z.ord 
         key = @keystream.get
         res = block.call(c, key)
         out << res.chr 
-        puts out
       else
         out << c.chr
       end
@@ -49,12 +48,12 @@ end
 class Deck
 
   def initialize
-    @deck = (1..52).to_a << :A << :B 
+    @deck = (1..52).to_a << :A << :B
   end
 
-  def number ( index )
-    return 53 if @deck[index] == :A or @deck[index] == :B
-    return @deck[index]
+  def number ( card )
+    return 53 if card == :A or card == :B
+    return card 
   end
 
   def top_joker
@@ -75,35 +74,67 @@ class Deck
 
   def move_down( index )
     temp = @deck[index]
+    neighbor_index = (index + 1) % 54
+    if neighbor_index == 0 
+      @deck = @deck[0,1] + @deck[@deck.length-1, 1] + @deck[1, @deck.length - 2]
+      return
+    end
     neighbor = @deck[ (index + 1) % 54 ]
     @deck[index] = neighbor
     @deck[ (index + 1) % 54 ] = temp
   end
 
   def triple_cut
-    first_cut_index = top_joker > 0 ? top_joker - 1 : 0
-    first_cut = first_cut_index > 0 ? @deck[0..first_cut_index] : []
+    do_first = top_joker > 0 ? true : false 
+    first_cut = do_first ? 
+                  @deck[0, top_joker] : 
+                  []
     second_cut = @deck[top_joker, bottom_joker - top_joker + 1]
-    third_cut = @deck[bottom_joker + 1,@deck.length - bottom_joker + 1]
+    do_third = bottom_joker < @deck.length - 1 ? true : false
+    third_cut = do_third ? 
+                  @deck[bottom_joker + 1, @deck.length - bottom_joker + 1] :
+                  []
+    #puts
+    #puts "first cut: #{first_cut}"
+    #puts "second cut: #{second_cut}"
+    #puts "third cut: #{third_cut}" 
+    #puts
+ 
     @deck = third_cut + second_cut + first_cut
-    puts "first cut: #{first_cut}"
-    puts "second cut: #{second_cut}"
-    puts "third cut: #{third_cut}" 
   end
   
+  def count_cut
+    cut_to = last = number( @deck.last )
+    return if cut_to == 53
+    top_cut = @deck[0, cut_to] 
+    in_between_cut = @deck[cut_to, @deck.length - cut_to - 1] 
+    bottom = @deck[-1,1]
+    @deck = in_between_cut + top_cut + bottom 
+  end
+
+  def output
+    ret = number( @deck[ number ( @deck[0] ) ] )
+    return if ret == 53
+    return 64 + ( number ( @deck[ number( @deck[0] ) ] ) % 26 )
+  end
+ 
   def update
-    puts @deck.join
+    #puts "before update: " + @deck.join(',')
     move_a
-    puts @deck.join
+    #puts "a moved:       " + @deck.join(',')
     move_b
-    puts @deck.join
+    #puts "b moved:       " + @deck.join(',')
     triple_cut
-    puts @deck.join
+    #puts "triple-cutted: " + @deck.join(',')
+    count_cut
+    #puts "count-cutted:  " + @deck.join(',')
+    #2.times { puts } 
   end
 
   def get
     update
-    return 67
+    return output if output 
+    get
   end
 
 end
@@ -113,4 +144,15 @@ msg = "Code in ruby, live longer!"
 puts msg
 e = Encrypter.new( Deck.new )
 enc = e.encrypt( msg )
-puts enc
+puts "encr: " + enc
+
+e_prime = Encrypter.new( Deck.new )
+puts "decr: " + e_prime.decrypt( enc )
+
+rq_msgs = [ "CLEPK HHNIY CFPWH FDFEH", "ABVAW LWZSY OORYK DUPVH" ] 
+
+rq_msgs.each do |m|
+  puts "encoded message : " + m 
+  e_prime = Encrypter.new( Deck.new )
+  puts "decoded message : " + e_prime.decrypt( m ) 
+end
