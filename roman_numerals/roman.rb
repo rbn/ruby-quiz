72fa17a69fd9dc076e06dd21d9258cf8dae1b008
rbn <richnyman@gmail.com>
2012-07-04 02:03:21 -0400

    numbers to numerals working

--- roman_numerals/roman.rb
+++ roman_numerals/roman.rb
@@ -15,6 +15,7 @@ class Roman
   attr_accessor :numerals
 
   def initialize( v )
+    # define hash of roman numerals and their numeric equivalents
     @numerals = {
       'I' => 1,
       'V' => 5,
@@ -24,7 +25,11 @@ class Roman
       'D' => 500,
       'M' => 1000
     }
+  
+    # base ten numbers that start with a 4 or 9 require numeral subtraction
+    @subtractors = [ 4, 9 ] 
 
+    # convert input to roman numeral
     convert(v)
   end
 
@@ -35,16 +40,11 @@ class Roman
       @numeral = v 
       @number = numberify(v)
     else
-      @numeral = romanify(v) 
+      @numeral = convert_to_roman(v) 
       @number = v
     end
   end
 
-  # given a numeric value n , return next highest roman numeral in hash
-  def upper(n)
-    curr = @numerals.invert[n]
-    curr #@numerals.keys[curr].next
-  end
 
   def numeral
     @numeral 
@@ -59,65 +59,91 @@ class Roman
   def romanify(i)
    # store original number as ORIG                                           # => 9
    orig = i
+   
    # store roman numeral as ROMAN (initialize as empty string)               # => ''
    roman = ''
+   
    # store multiplier as MULT = 10**( (no. columns right of 0) - 1 )         # => 1
    mult = 10**(i.to_s.length - 1)
+   
    # store current floor as FLOOR = ORIG >= 5 * MULT ? 5 * MULT : MULT       # => 5
    floor = orig >= ( 5 * mult ) ? 
                      ( 5 * mult )  : 
                      mult
+   
    # repeater = ORIG / FLOOR                                                 # => 1 
    repeater = orig / floor
+   
    # ROMAN += @numerals.invert[FLOOR]                                        # => 'V'
-   roman += get(floor) * repeater 
+   roman += @numerals.invert[floor] * repeater 
+   
    # store REMAINDER = ORIG - FLOOR                                          # => '4' 
    remainder = orig - ( floor * repeater )
+   
    # if REMAINDER, append romanify(remainder)                                # => 'VIIII'
    if remainder > 0
     roman += romanify(remainder)
    end
-   # if character repeats more than three times in ROMAN
-   if repeats(roman)
-     # store next_num(FLOOR) in NEXT_NUM                                       # => 'X'
-     upper_numeral = upper(floor) 
-     # store next_val(FLOOR) in NEXT_VAL                                       # => '10'  
-     upper_val = @numerals[upper_numeral]
-     # store SUBTR as NEXT_VAL - ORIG                                          # => '1'
-     subtr = upper_val - orig
-     # ROMAN += romanify(SUBTR) + NEXT_VAL                                     # => 'IX'
-     roman = upper_numeral #romanify(subtr) + upper
-   end
+   
    roman
   end
 
-
-  def numberify(n)
-  end
-
-  def repeats(s)
-    s = s.gsub(/[^A-Za-z]/, '')
-    # TODO - find a better way to check for three of the same chars in a row
-    last = '', counter = 0
-    s.split('').each do |c|
-      counter = ( last == c ) ?
-                counter + 1 :
-                0
-      return true if counter == 3 
-      last = c
+  def convert_to_roman(n)
+    roman = ''
+    s = n.to_s                                # => '1493'
+    s = s.split('').reverse                   # => '[ 3, 9, 4, 1 ]'
+    s.each_with_index do | value, index |
+      power = index
+      base_ten = ( value.to_i * ( 10 ** power ) ).to_s  
+      roman.insert( 0, get_base_ten_numeral( base_ten ) )
     end
-    return false
+    roman
   end
 
-  # given a numeric value n, return the corresponding roman numeral in hash
-  def get(n)
-    @numerals.invert[n] 
+  # given a number base 10, return the corresponding numeral
+  def get_base_ten_numeral(s)
+    n = s.to_i
+    # examine left most character
+    left = s[0,1]
+    # if 4 or 9, subtract from upper bound 
+    return @subtractors.include?(left.to_i) ?
+             subtract(n) : 
+             romanify(n)
+  end
+
+  def upper(n)
+    s = n.to_s
+    power = s.length - 1
+    upper = 10 * ( 10 ** power )
+    return @numerals.invert[upper]
+  end
+
+  def mid(n)
+    s = n.to_s
+    power = s.length - 1
+    mid = 5 * ( 10 ** power )
+    return @numerals.invert[mid]
+  end
+
+  def lower(n)
+    s = n.to_s
+    power = s.length - 1
+    lower = 1 * ( 10 ** power )
+    return @numerals.invert[lower]
+  end
+
+  def subtract(n)
+    s = n.to_s
+    power = s.length - 1
+    mid = 5 * ( 10 ** power )
+    return n > mid ?
+             lower(n) + upper(n) :
+             lower(n) + mid(n)
   end
 end
 
 # OUTPUT
-r = Roman.new(9)
-puts "9 converts to #{r.numeral}"
-#puts "III converts to #{r.convert('III')}"
+r = Roman.new(1493)
+puts "1493 converts to #{r.numeral}"
 
 
